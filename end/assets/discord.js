bf84a509e1c5c302f6944f1b506186af81ddc6c3
Vojtěch Döme <ojta@post.cz>
2022-03-29 08:23:49 +0200

    Contact relink n copy

---
+++ end/assets/discord.js
@@ -0,0 +1,18 @@
+const discord = document.querySelector('#discord');
+const userName = "Vojtysek#9079";
+const discordText = document.querySelector('#discordText');
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+discord.addEventListener("click", () => {
+    navigator.clipboard.writeText(userName);
+    discordText.innerHTML = "Copied!";
+    delay(1000).then(() => 
+        discordText.innerHTML = "Discord"
+    );
+});
+
+//Finished
+// Language: javascript
